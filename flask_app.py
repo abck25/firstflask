18d64956519b91db8ba2f54d9ab8241a2b9fa5c1
Abi <k.abiraam@gmail.com>
2022-01-23 10:11:22 +0100

    fake data added

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,6 +8,14 @@ def hello_world():
     return "<p>Hello, from TechLabs!</p>"
 
 
-@app.route("/hello/")
-def hello():
-    return f"<p>hello!</p>"
+wines = [
+    {"id": 0, "alcohol": 8, "quality": 10},
+    {"id": 1, "alcohol": 12, "quality": 8},
+    {"id": 2, "alcohol": 10.5, "quality": 9},
+]
+
+
+
+@app.route("/api/wines/all", methods=["GET"])
+def return_all():
+    return jsonify(wines)
